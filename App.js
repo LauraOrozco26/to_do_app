8666b6e0f3b9a0dbb414da96c229aa1d7ff70c29
JeannethMedrano14 <141864876+JeannethMedrano14@users.noreply.github.com>
2024-01-05 12:08:05 -0500

    Inicial

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Notas from "./screens/Notas";
+import CreateNote from "./screens/CreateNote";
+import DetailsNote from "./screens/DetailsNote";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notas"
+          component={Notas}
+          options={{
+            title: "ToDoApp",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#000" },
+            headerTintColor: "white",
+          }}
+        />
+        <Stack.Screen
+          name="Crear"
+          component={CreateNote}
+          options={{
+            title: "Crear Notas",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#129BF4" },
+            headerTintColor: "white",
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailsNote}
+          options={{
+            title: "Detalles de nota",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#000" },
+            headerTintColor: "white",
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+    justifyContent: "center",
+    tifyContent: "center",
+    tifyContent: "center",
   },
 });
